309ed68029f22d5da1bd8ecd5b939fe14a16393a
Clever dos Anjos <clever@clever.com.br>
2018-11-21 20:18:59 -0200

    fixing owner name

--- import-user-apps.ps1
+++ import-user-apps.ps1
@@ -7,5 +7,6 @@ foreach ($qvf in gci -Recurse -Filter "*.qvf" -Path $folder ) {
     $app = $qvf.BaseName
     $user =  $qvf.Directory.Name
     $app = Import-QlikApp -file $qvf.FullName -name "$app" -upload
-    Update-QlikApp -id $app.id -ownername $user 
+    $u = Get-QlikUser -filter "userId eq '$user'"
+    Update-QlikApp -id $app.id -ownername $u.name 
 }
