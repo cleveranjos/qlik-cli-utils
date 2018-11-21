9f91fd6d794c7a3d067fdfb6a7ebb10969fc161f
Clever dos Anjos <clever@clever.com.br>
2018-11-21 19:22:21 -0200

    Create export-pub-apps.ps1

---
+++ export-pub-apps.ps1
@@ -0,0 +1,13 @@
+#
+# Dump published apps
+# a folder for each stream
+# 
+$folder = "c:\dump"
+
+Connect-Qlik r ## check https://github.com/ahaydon/Qlik-Cli for details
+
+foreach($qvf in Get-QlikApp -filter "Published eq true") {
+    $streamfolder = $qvf.stream.name
+    New-Item -ItemType Directory -Force -Path "$folder\$streamfolder"
+    Export-QlikApp -id $qvf.id -filename "$($folder)\$($streamfolder)\$($qvf.name).qvf" #dumps the qvf
+}
