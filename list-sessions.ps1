bd3d46abf4fa3c7e6a17ab178506fefb948dfc7d
Clever dos Anjos <clever@clever.com.br>
2018-12-26 18:58:01 -0200

    Add kill capability

--- list-sessions.ps1
+++ list-sessions.ps1
@@ -7,8 +7,22 @@ foreach ($vp in Get-QlikVirtualProxy) {
         UserId = $session.UserId
         VirtualProxy  =  $vp.description        
         SessionId = $session.SessionId
+        VirtualProxyPrefix = $vp.prefix
         }
     $sessions.Add($s)|Out-Null
    } 
 }
 $sessions | Format-Table
+
+$value = $(Read-Host -Prompt 'Choose a session to terminate') -as [int]
+if (($value -gt 0) -and ($value -le $sessions.Count)) {
+    $yn = Read-Host -Prompt "Are you sure you want to terminate session [$value] - [$($sessions.Item($value-1).SessionId)] (y/n)?"
+    if($yn -match "[yY]") {
+        $proxy = Get-QlikProxy local
+		$prefix = "https://$($proxy.serverNodeConfiguration.hostName):$($proxy.settings.restListenPort)/qps"
+        if($sessions.Item($value-1).VirtualProxyPrefix) {
+             $prefix = "$($prefix)/$($sessions.Item($value-1).VirtualProxyPrefix)"
+        }
+        Invoke-QlikDelete -path "$prefix/session/$($sessions.Item($value-1).SessionId)"
+    }
+}
