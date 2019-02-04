ae1ddadc89e6aec853a182e98f5d90f017b3eb4d
Clever Anjos <CUV@qlik.com>
2019-02-04 19:08:22 -0200

    Add MIT License

--- list-sessions.ps1
+++ list-sessions.ps1
@@ -1,4 +1,28 @@
-Connect-Qlik|Out-Null
+<#
+MIT License
+
+Copyright (c) 2018 Clever dos Anjos
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+#>
+Connect-Qlik|Out-Null ## check https://github.com/ahaydon/Qlik-Cli for details
 [System.Collections.ArrayList]$sessions = @()
 foreach ($vp in Get-QlikVirtualProxy) {
     foreach ($session in Get-QlikSession -virtualProxyPrefix $vp.prefix) {
--- purge-old-apps.ps1
+++ purge-old-apps.ps1
@@ -1,4 +1,6 @@
 <#
+MIT License
+
 Copyright (c) 2018 Clever dos Anjos
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,6 +20,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
 #>
 $backupFolder = "C:\QlikBackup" 
 Function LogWrite {
